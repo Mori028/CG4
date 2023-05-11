420bc24e215d3918a5363e6a80ed0d4c7c2c78f1
Moriatsushi <k021g1463@g.neec.ac.jp>
2023-05-11 11:14:58 +0900

    01_08

--- DirectXGame/Engin/3d/FbxLoader.h
+++ DirectXGame/Engin/3d/FbxLoader.h
@@ -16,6 +16,8 @@ private://エイリアス
 public://定数
 	//モデル格納ルートパス
 	static const string baseDirectory;
+	// テクスチャがない場合の標準テクスチャファイル名
+	static const string defaultTextureFileName;
 
 	/// <summary>
 	/// ファイルからFBXモデル読み込み
@@ -43,6 +45,19 @@ public:
 	/// 再帰的にノード構成を解析 
 	/// </summary>
 	void ParseNodeRecursive(Model* model, FbxNode* fbxNode,Node* parent = nullptr);
+
+	/// <summary>
+	/// メッシュ読み取り
+	/// </summary>
+	void ParseMesh(Model* model, FbxNode* fbxNode);
+	// 頂点座標読み取り
+	void ParseMeshVertices(Model* model, FbxMesh* fbxMesh);
+	// 面情報読み取り
+	void ParseMeshFaces(Model* model, FbxMesh* fbxMesh);
+	// マテリアル読み取り
+	void ParseMaterial(Model* model, FbxNode* fbxNode);
+	// テクスチャ読み込み
+	void LoadTexture(Model* model, const std::string& fullpath);
 private:
 	// privateなコンストラクタ（シングルトンパターン）
 	FbxLoader() = default;
@@ -58,4 +73,6 @@ private:
 	FbxManager* fbxManager = nullptr;
 	//FBXインポータ―
 	FbxImporter* fbxImporter = nullptr;
+	// ディレクトリを含んだファイルパスからファイル名を抽出する
+	std::string ExtractFileName(const std::string& path);
 };
--- DirectXGame/Engin/3d/Model.h
+++ DirectXGame/Engin/3d/Model.h
@@ -1,5 +1,10 @@
+#include <Windows.h>
+#include <wrl.h>
+#include <d3d12.h>
+#include <d3dx12.h>
 #include <string>
 #include <DirectXMath.h>
+#include <DirectXTex.h>
 
 // ノード
 struct Node
@@ -21,13 +26,39 @@ struct Node
 };
 
 class Model {
+
 public:
 	//フレンドクラス
 	friend class FbxLoader;
+
+	struct VertexPosNormalUv {
+		DirectX::XMFLOAT3 pos;
+		DirectX::XMFLOAT3 normal;
+		DirectX::XMFLOAT2 uv;
+	};
+
+	// バッファ生成
+	void CreateBuffers(ID3D12Device* device);
+	// 描画
+	void Draw(ID3D12GraphicsCommandList* cmdList);
 
 private:
 	//モデル名
 	std::string name;
 	//ノード配列
 	std::vector<Node> nodes;
+	//メッシュを持つノード 
+	Node* meshNode = nullptr;
+	//頂点データ配列
+	std::vector<VertexPosNormalUv> vertices;
+	//頂点インデックス配列
+	std::vector<unsigned short> indices;
+	// アンビエント係数
+	DirectX::XMFLOAT3 ambient = { 1,1,1 };
+	// ディフューズ係数
+	DirectX::XMFLOAT3 diffuse = { 1,1,1 };
+	// テクスチャメタデータ
+	DirectX::TexMetadata metadata = {};
+	// スクラッチイメージ
+	DirectX::ScratchImage scratchImg = {};
 };
